a13607aa1b897f88da89f91897730734e4a5c857
Vaibhav Sharma <vsvaibhav2016@gmail.com>
2021-06-27 17:29:40 +0530

    postfix to infix

---
+++ Stack/conversion-from-postfix/postfix-to-infix.js
@@ -0,0 +1,51 @@
+class Stack {
+  constructor() {
+    this.items = [];
+  }
+
+  push(data) {
+    this.items.push(data);
+  }
+
+  pop() {
+    return this.items.pop();
+  }
+
+  isEmpty() {
+    return this.items.length === 0 ? true : false;
+  }
+
+  top() {
+    return this.items[this.items.length - 1];
+  }
+}
+
+
+const PostfixToInfix = (postfixString) => {
+  let stack = new Stack();
+  const postfixStrArr = postfixString.split("");
+  postfixStrArr.forEach((char, index) => {
+    if (!isOperator(char)) {
+      stack.push(char);
+    } else if (isOperator(char)) {
+      let str1 = stack.pop();
+      let str2 = stack.pop();
+      const newStr = `(${str1}${char}${str2})`;
+      stack.push(newStr);
+    }
+  });
+  return stack.top();
+};
+
+
+const isOperator = (char) => {
+  if (
+    char === "+" ||
+    char === "-" ||
+    char === "*" ||
+    char === "/" ||
+    char === "^"
+  )
+    return true;
+  return false;
+};
