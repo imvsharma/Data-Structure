d29601ce73a8eafa6b391c58a777179b5128f784
Vaibhav Sharma <vsvaibhav2016@gmail.com>
2021-06-27 16:51:29 +0530

    infix to postfix conversion

---
+++ Stack/conversion-from-infix/infix-to-postfix.js
@@ -0,0 +1,54 @@
+class Stack {
+  constructor() {
+    this.items = [];
+  }
+
+  push(data) {
+    this.items.push(data);
+  }
+
+  pop() {
+    return this.items.pop();
+  }
+
+  isEmpty() {
+    return this.items.length === 0 ? true : false;
+  }
+
+  top() {
+    return this.items[this.items.length - 1];
+  }
+}
+
+const infixToPostfix = (infixString) => {
+  // create empty stack
+  let stack = new Stack();
+  // create empty result string
+  let postfixStr = "";
+  // convert string into array
+  const infixStrArr = infixString.split("");
+
+  infixStrArr.forEach((char) => {
+    if ( (char >= "a" && char <= "z") || (char >= "A" && char <= "Z") || (char >= "0" && char <= "9") ) {
+      postfixStr += char;
+    } else if (char === "(") {
+      stack.push(char);
+    } else if (char === ")") {
+      while (stack.top() !== "(") {
+        postfixStr += stack.pop();
+      }
+      stack.pop();
+    } else if ( char === "^" || char === "/" || char === "*" || char === "+" || char === "-" ) {
+      while ( char !== "^" && !stack.isEmpty() && getPrecendence(char) <= getPrecendence(stack.top()) ) {
+        postfixStr += stack.pop();
+      }
+      stack.push(char);
+    }
+  });
+
+  while (!stack.isEmpty()) {
+    postfixStr += stack.pop();
+  }
+
+  return postfixStr;
+};
