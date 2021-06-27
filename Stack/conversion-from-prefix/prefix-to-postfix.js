0be47d6b300be3b1580e52d0af1863cff7252081
Vaibhav Sharma <vsvaibhav2016@gmail.com>
2021-06-27 17:24:19 +0530

    prefix to postfix

---
+++ Stack/conversion-from-prefix/prefix-to-postfix.js
@@ -0,0 +1,38 @@
+class Stack {
+  constructor() {
+    this.items = [];
+  }
+
+  push(data) {
+    this.items.push(data);
+  }
+
+  pop() {
+    return this.items.pop();
+  }
+
+  isEmpty() {
+    return this.items.length === 0 ? true : false;
+  }
+
+  top() {
+    return this.items[this.items.length - 1];
+  }
+}
+
+const prefixToInfix = (prefixString) => {
+  let stack = new Stack();
+  const revPrefixString = prefixString.split("").reverse();
+
+  revPrefixString.forEach((char) => {
+    if (!isOperator(char)) {
+      stack.push(char);
+    } else {
+      let operand1 = stack.pop();
+      let operand2 = stack.pop();
+      const str = `${operand1}${operand2}${char}`;
+      stack.push(str);
+    }
+  });
+  return stack.top();
+};
